0aa2bbc15898970183e57d65723301bbb4f06ccc
OrlandoTele96 <jorge.o.gonzalez.dev@gmail.com>
2021-05-22 18:30:30 -0500

    Bottom & top bar

--- header.php
+++ header.php
@@ -6,29 +6,62 @@
         private $bottombar = [];
         public function getHeader(){
             $content = '';
-            foreach ($this->topbar as $section)
+            $content_bottom = '';
+            foreach ($this->topbar as $section){
                 $content = $content.$section;
-            return $this->opentag.$content.$this->closetag;
+                }
+            foreach($this->bottombar as $sectionb){
+                $content_bottom = $content_bottom.$sectionb;
+            }
+            return $this->opentag.$content.$content_bottom.$this->closetag;
         }
         public function setTopBar(){
             $open = '<div class="top-bar">';
             $brand = $this->getImageBrand();
+            $topmenu = $this->getTopMenu();
             $close = '</div>';
             array_push($this->topbar,$open);
             array_push($this->topbar,$brand);
+            array_push($this->topbar,$topmenu);
             array_push($this->topbar,$close);
         }
-        /*public function setBottomBar (){
-
-        }*/
+        public function setBottomBar (){
+            $open = '<div class="bottom-bar">';
+            $menu_b= $this->getBottomMenu();
+            $close = '</div>';
+            array_push($this->bottombar,$open);
+            array_push($this->bottombar,$menu_b);
+            array_push($this->bottombar,$close);
+        }
         public function getImageBrand(){
             $open = '<div class="cianabrand">';
             $Image = '<img src='.'"assets/img/brand.png"'.' alt="">';
             $close = '</div>';
             return $open.$Image.$close;
         }
-        /*public fuction getTopMenu(){
+        public function getTopMenu(){
+            $nav_menu_open = '<nav>';
+            $topmenu = '';
+            $openitem = '<li><a href="Perfil.html">';
+            $closeitem = '</a></li>';
+            $menu = array('Mi perfil','Mi lista de deseos','Mi carrito');
+            foreach($menu as $item)
+                $topmenu = $topmenu.$openitem.$item.$closeitem;
+            $nav_menu_close='</nav>';
+            return $nav_menu_open.$topmenu.$nav_menu_close;
+        }
+        public function getBottomMenu(){
+            $open_menu = '<nav>';
+            $bottommenu='';
+            $openitem = '<li><a href="index.html">';
+            $menu = array('Inicio','Novedades','Acerca de');
+            $closeitem = '</a></li>';
+            foreach($menu as $item)
+                $bottommenu= $bottommenu.$openitem.$item.$closeitem;
 
-        }*/
+            
+            $close_menu = '</nav>';
+            return $open_menu.$bottommenu.$close_menu;
+        }
     } 
 ?>
--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
   include 'header.php';
   $header = new  HeaderPage();
-  $header->getImageBrand();
+  //$header->getImageBrand();
+  //$header->getTopMenu();
+  //echo $header->getBottomMenu();
   $header->setTopBar();
+  $header->setBottomBar();
   $head_section=$header->getHeader();
   require 'base.view.php';
 ?>
